62983f4441ff58cb437a68a1fccce5b70c2e3150
Craig Eddy <craig.eddy@gmail.com>
2026-06-04 19:55:10 -0400

    Enhance main function output with messages for dog and cat creation

--- main.go
+++ main.go
@@ -1,11 +1,16 @@
 // Package main demonstrates the animals package by creating and interacting with Dog and Cat instances.
 package main
 
-import "hello_go/animals"
+import (
+	"fmt"
+	"hello_go/animals"
+)
 
 func main() {
 	dog := animals.NewDog("Rex")
+	fmt.Printf("Create a new dog named %s\n", dog.Name())
 	cat := animals.NewCat("Whiskers")
+	fmt.Printf("Create a new cat named %s\n", cat.Name())
 	dog.Talk()
 	cat.Talk()
 }
